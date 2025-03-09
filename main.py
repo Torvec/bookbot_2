4e39c25f45583264337337688defa9b29db66e5e
vonedward <me@edward-vonschondorf.dev>
2025-03-08 17:06:41 -0800

    Add ability to analyze any book in text format via cli input

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import sys
 from stats import get_num_words, get_num_chars, list_char_counts, sort_list
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    file_path = sys.argv[1]
     book_text = get_book_text(file_path)
     word_count = get_num_words(book_text)
     char_count = get_num_chars(book_text)
